6649600b54b9f83734736e8b923bfa0669e2974a
Eitan Rousso <rousso@gmail.com>
2023-11-12 12:22:17 +0200

    aws sdk v3

--- test-save-to-s3.js
+++ test-save-to-s3.js
@@ -1,39 +1,34 @@
-import { S3 } from "@aws-sdk/client-s3";
-const s3 = new S3();
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 
-const handler = async () => {
-  // Extract data from the API Gateway request
-  const requestBody = { a: 1, b: 'bbb', c: 'sample' };
+async function uploadFileToS3() {
+  const bucketName = 'ej-data-lake'; // Replace 'your-s3-bucket-name' with your S3 bucket name
   const channelId = Math.random().toString().substring(2);
   const messageId = Math.random().toString().substring(2);
   const timestamp = new Date().getTime();
   const s3Key = `/test/channels/${channelId}/message/${messageId}/${timestamp}/1/1.json`;
+  const requestBody = { a: 1, b: 'bbb', usedS3Key: s3Key };
+
+  // Create an S3 client
+  const s3Client = new S3Client({ region: 'your-region' }); // Replace 'your-region' with your AWS region
+
+  // Create the parameters for the S3 upload
+  const uploadParams = {
+    Bucket: bucketName,
+    Key: s3Key,
+    Body: requestBody,
+  };
+
   try {
-    // Create parameters for the S3 PutObject operation
-    const s3Params = {
-      Bucket: 'ej-data-lake',
-      Key: s3Key,
-      Body: JSON.stringify(requestBody),
-    };
+    // Upload the file to S3
+    const command = new PutObjectCommand(uploadParams);
+    const response = await s3Client.send(command);
 
-    // Upload the JSON data to the S3 bucket
-    await s3.putObject(s3Params);
-
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({ message: `saved successfully: ${s3Key}` }),
-    };
-    return response;
-  } catch (error) {
-    console.error('Error:', error);
-    const response = {
-      statusCode: 500,
-      body: JSON.stringify({ message: `Error saving data to S3 (${s3Key})` }),
-    };
-    return response;
+    console.log(`File uploaded successfully. ETag: ${response.ETag}`);
+    console.log(s3Key);
+  } catch (err) {
+    console.error('Error uploading file to S3:', err);
+    console.log(s3Key);
   }
-};
+}
 
-(async () => {
-  await handler();
-})();
+uploadFileToS3();
