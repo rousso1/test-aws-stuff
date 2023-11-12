8c1c1bb72398bafa35d51fd6e3d1755f6a12a0cd
Eitan Rousso <rousso@gmail.com>
2023-11-12 12:26:06 +0200

    update region

--- test-save-to-s3.js
+++ test-save-to-s3.js
@@ -1,17 +1,15 @@
 const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 
 async function uploadFileToS3() {
-  const bucketName = 'ej-data-lake'; // Replace 'your-s3-bucket-name' with your S3 bucket name
+  const bucketName = 'ej-data-lake';
   const channelId = Math.random().toString().substring(2);
   const messageId = Math.random().toString().substring(2);
   const timestamp = new Date().getTime();
   const s3Key = `/test/channels/${channelId}/message/${messageId}/${timestamp}/1/1.json`;
   const requestBody = { a: 1, b: 'bbb', usedS3Key: s3Key };
 
-  // Create an S3 client
-  const s3Client = new S3Client({ region: 'your-region' }); // Replace 'your-region' with your AWS region
+  const s3Client = new S3Client({ region: 'eu-central-1' });
 
-  // Create the parameters for the S3 upload
   const uploadParams = {
     Bucket: bucketName,
     Key: s3Key,
